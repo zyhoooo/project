1c80e4a42a613ca448960670279e6d298a26e188
Loard <2510642258@qq.com>
2021-12-02 22:52:43 +0800

    first test commit

---
+++ src/test1.java
@@ -0,0 +1,12 @@
+/**
+ * @Description TODO
+ * @ClassName test1
+ * @Author 钟宇豪
+ * @Date 2021/12/2 20:50
+ * @Version 1.0
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
