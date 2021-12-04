3d5c2813675863975fab383009da6fee9f2f4785
zyhoooo <2510642258@qq.com>
2021-12-04 10:31:36 +0800

    first test commit

--- src/test1.java
+++ src/test1.java
@@ -9,5 +9,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("push git");
     }
 }
